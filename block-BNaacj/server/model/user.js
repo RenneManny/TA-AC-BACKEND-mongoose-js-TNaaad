35f16f470a28f6c4d458b103040dae07991d643a
Akhil <akhilkum249@gmail.com>
2024-03-23 16:17:47 +0530

    block-BNaa

--- block-BNaacj/server/model/user.js
+++ block-BNaacj/server/model/user.js
@@ -1,6 +1,6 @@
 // writeCode
 
-// Q. Update user schema to
+// Q. Upd ate user schema to
 
 // - contains password field with minimum 5 characters and maximim 15
 // - add createdAt field of type date and default it to current date.
